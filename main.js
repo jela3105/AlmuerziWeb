4888fad6be396dc162e1a8146d24e75fe8efc12b
jela3105 <jela3105@gmail.com>
2021-02-02 15:27:35 -0600

    Save the id meal in the form

--- main.js
+++ main.js
@@ -1,23 +1,3 @@
-const stringToHTML = (stringText) => {
-  const parser = new DOMParser();
-  const doc = parser.parseFromString(stringText, "text/html");
-  return doc.body.firstChild;
-};
-
-const renderItem = (item) => {
-  const element = stringToHTML(`<li data_id="${item._id}">${item.name}</li>`);
-
-  element.addEventListener("click", () => {
-    const mealsList = document.getElementById("meals-list");
-    Array.from(mealsList.children).forEach((x) =>
-      x.classList.remove("selected")
-    );
-    element.classList.add("selected");
-  });
-
-  return element;
-};
-
 window.onload = () => {
   fetch("https://serverless.jela3105.vercel.app/api/meals")
     .then((response) => response.json())
@@ -26,9 +6,29 @@ window.onload = () => {
       const submit = document.getElementById("submit");
       const itemList = data.map(renderItem);
 
-      mealsList.removeChild(mealsList.firstElementChild);
+      mealsList.removeChild(mealsList.firstElementChild); //remove loading text
       itemList.forEach((element) => mealsList.appendChild(element));
 
       submit.removeAttribute("disabled");
     });
+};
+
+const renderItem = (item) => {
+  const element = stringToHTML(`<li data_id="${item._id}">${item.name}</li>`);
+  element.addEventListener("click", () => {
+    const mealsList = document.getElementById("meals-list");
+    Array.from(mealsList.children).forEach((x) =>
+      x.classList.remove("selected")
+    );
+    element.classList.add("selected");
+    const mealsIdInput = document.getElementById("meals-id");
+    mealsIdInput.value = item._id;
+  });
+  return element;
+};
+
+const stringToHTML = (stringText) => {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(stringText, "text/html");
+  return doc.body.firstChild;
 };
